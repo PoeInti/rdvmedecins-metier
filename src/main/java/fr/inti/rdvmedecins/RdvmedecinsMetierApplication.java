a5a43cd005f9af12c8c6cfc02f72c02eaaf68b36
Poe Data <poe.data.inti@gmail.com>
2020-03-23 09:38:53 +0100

    first commit

---
+++ src/main/java/fr/inti/rdvmedecins/RdvmedecinsMetierApplication.java
@@ -0,0 +1,13 @@
+package fr.inti.rdvmedecins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdvmedecinsMetierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdvmedecinsMetierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/inti/rdvmedecins/RdvmedecinsMetierApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.inti.rdvmedecins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdvmedecinsMetierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
